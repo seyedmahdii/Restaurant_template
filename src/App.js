1be2cf27188ce69adc6548398e8ece0065d9b8f1
Seyed Mahdi Jalali <seyedmahdijalali2020@gmail.com>
2021-09-13 22:46:31 +0430

    Loading.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Features from "./components/Features/Features";
 import Foods from "./components/Foods/Foods";
 import Footer from "./components/Footer/Footer";
+import Loading from "./components/Loading/Loading";
 
 function App() {
+    const [isLoading, setIsLoading] = useState(true);
+    window.addEventListener("load", () => {
+        setIsLoading(false);
+    });
+    if (isLoading) {
+        return <Loading />;
+    }
+
     return (
         <div className="app">
             <Header />
